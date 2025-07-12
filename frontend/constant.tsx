a3c80097c175216fc51081e473c8f0f145bcc591
AmanJaiswal <jaman0120@gmail.com>
2025-07-13 00:01:28 +0530

    feat: fix environment variable names for base URL in constant.tsx

--- frontend/constant.tsx
+++ frontend/constant.tsx
@@ -1,4 +1,4 @@
-export const baseUrl = process.env.NEXT_NODE_ENV === 'development' 
+export const baseUrl = process.env.NEXT_PUBLIC_NODE_ENV === 'development' 
 ? 'http:teacher-management-gydfaaesaaebe8hr.centralindia-01.azurewebsites.net/api/v1' // Adjust port to match your backend
-: process.env.NEXT_BASE_URL || '';
+: process.env.NEXT_PUBLIC_BASE_URL || '';
 
